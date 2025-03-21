dd6686a3b83d71ed55aeeae32f45ecbd932cf30e
Anton Donchenko <donch.a080@gmail.com>
2025-03-21 15:25:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,7 @@
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
-spider.style.top = wall.clientHeight / 2 - spider.height / 2 + 'px';
-spider.style.left = wall.clientWidth / 2 - spider.width / 2 + 'px';
+spider.addEventListener('load', () => {
+  spider.style.top = wall.clientHeight / 2 - spider.offsetHeight / 2 + 'px';
+  spider.style.left = wall.clientWidth / 2 - spider.offsetWidth / 2 + 'px';
+});
